5f1f02e98c27e3282784d51fdf7c8ec5cb3b94c1
wuyaheng <wuyaheng2016@gmail.com>
2021-02-10 21:51:28 -0500

    change checkbox check color

--- src/components/AppointmentForm/index.js
+++ src/components/AppointmentForm/index.js
@@ -5,19 +5,19 @@ function AppointmentForm({ results, handleInputChange }) {
         <div className="card p-2 pb-0">
         <div className="pt-1">
         <label>
-            <input type="checkbox" class="filled-in"/>
+            <input type="checkbox" class="filled-in checkbox-blue"/>
             <span>Both</span>
         </label>
         </div>
         <div>
         <label>
-            <input type="checkbox" class="filled-in" />
+            <input type="checkbox" class="filled-in checkbox-blue" />
             <span>Walk-in Only</span>
         </label>
         </div>
         <div>
         <label className="mb-0">
-            <input type="checkbox" class="filled-in" />
+            <input type="checkbox" class="filled-in checkbox-blue" />
             <span>Appointment Only</span>
         </label>
         </div>
--- src/components/CheckForm/index.js
+++ src/components/CheckForm/index.js
@@ -5,13 +5,13 @@ function CheckForm({ results, handleInputChange }) {
     <div className="card p-2 pb-0">
         <div className="pt-1">
         <label>
-            <input type="checkbox" class="filled-in"/>
+            <input type="checkbox" class="filled-in checkbox-blue"/>
             <span>Yes</span>
         </label>
         </div>
         <div>
         <label className="mb-0">
-            <input type="checkbox" class="filled-in" />
+            <input type="checkbox" class="filled-in checkbox-blue" />
             <span>No</span>
         </label>
         </div>
